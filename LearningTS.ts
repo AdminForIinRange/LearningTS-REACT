5a169a7eabf8f7c4e206e939b0789e449170ae7e
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-20 23:00:48 +1030

    LearningTS+React

---
+++ LearningTS.ts
@@ -0,0 +1,11 @@
+
+let string: string;
+let boolean: boolean;
+let stringOrNumber: string | number;
+
+string = "hello";
+boolean = true;
+
+stringOrNumber = 123;
+
+console.log(string, boolean, stringOrNumber);
