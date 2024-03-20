791a342498c95b76cc2d853fc2b4467487d3cfdc
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-20 23:52:40 +1030

    playing around with generics and interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  let Hi : boolean
-  Hi = false
+  let Hi: string;
+  Hi = "hi";
 
+  const returnHi = (str: string, boolean: true): string => {
+    return str + boolean; //StrictMode is applied, so it will console twice
+  };
 
-  const returnHi = (): string =>{
- 
-    return "Hi"
+  interface UserData<T extends object> {
+    employee: T[];
   }
+  genreics
+  const EmployeeData: UserData<{
+    username: string;
+    UserID: number;
+    Role: string;
+  }> = {
+    employee: [
+      {
+        username: "1",
+        UserID: 1,
+        Role: "string",
+      },
+      {
+        username: "2",
+        UserID: 2,
+        Role: "string",
+      },
+      {
+        username: "3",
+        UserID: 3,
+        Role: "string",
+      },
+      {
+        username: "4",
+        UserID: 4,
+        Role: "string",
+      },
+    ],
+    
+  };
 
   return (
     <>
-    {console.log(Hi)}
-    {returnHi}
-      
+      {console.log(Hi)}
+      {returnHi("hi ", true)}
+      {console.log(EmployeeData)}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
