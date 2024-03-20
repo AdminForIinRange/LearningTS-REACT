eaf058ae58fcc35b7f7ea5015fec4cf238422d8f
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-20 23:07:37 +1030

    Checking if TS is working as intended

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  let Hi : boolean
+  Hi = false
+
+
   return (
     <>
-      
+    {console.log(Hi)}
+      s
     </>
   )
 }
