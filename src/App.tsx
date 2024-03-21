6ab928fd0dbf9200fbae8ee3b824d0261a4b5d01
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-21 16:25:43 +1030

    Starting Fresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,7 @@
-import { useState } from "react";
-import PostList from "./components/PostList/PostList.tsx"
-import PostCard from "./components/PostCard/PostCard.tsx"
-import "./App.css";
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  let Hi: string;
-  Hi = "hi";
-
-  const returnHi = (str: string, boolean: true): string => {
-    return str + boolean; //StrictMode is applied, so it will console twice
-  };
-
-  interface UserData<T extends object> {
-    employee: T[];
-  }
-
-  const EmployeeData: UserData<{
-    username: string;
-    UserID: number;
-    Role: string;
-  }> = {
-    employee: [
-      {
-        username: "1",
-        UserID: 1,
-        Role: "string",
-      },
-      {
-        username: "2",
-        UserID: 2,
-        Role: "string",
-      },
-      {
-        username: "3",
-        UserID: 3,
-        Role: "string",
-      },
-      {
-        username: "4",
-        UserID: 4,
-        Role: "string",
-      },
-    ],
-  };
-
+export default function App() {
   return (
-    <>
-
-     <PostCard />
-    </>
-  );
+    <div>App</div>
+  )
 }
-
-export default App;
