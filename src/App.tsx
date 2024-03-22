7604ae0a0912e40719086e50e05b561d6be7b0c6
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-22 13:16:05 +1030

    Mapping todo, and testing returning it to see if everything is running as expected.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import React, { useState } from "react";
 import "./App.css";
-import InputFeild from "./components/InputFeild";
+import InputField from "./components/InputFeild"; // Fixed typo in component import
 import { Todo } from "./model";
+
+/**
+ * App component represents the main application.
+ * It manages the state of todos and handles todo addition.
+ */
 const App: React.FC = () => {
+  // State to store the current todo input value
   const [todo, setTodo] = useState<string>("");
+
+  // State to store the list of todos
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  /**
+   * Handles the addition of a new todo item.
+   * @param e React form event
+   */
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (todo) {
+    // Check if the todo input is not empty
+    if (todo) { // On submit, it will first check if there's even a todo in the input field given to us by the onChange={setTodo}, then only will it run. This is self-evident when you have nothing in the field and you try to press go.
+      // Add the new todo to the todos list with a unique ID and mark it as not done
       setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      
+      // Clear the todo input field after adding the todo
       setTodo("");
+
+      // Log the updated todos list
+      console.log(todos);
     }
   };
+
   return (
     <div className="App">
+      {/* Application title */}
       <span className="heading">Taskify</span>
 
-      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* InputField component to add new todos */}
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+
+      {/* Display the list of todos */}
+      <ul>
+        {todos.map((t) => (
+          <div key={t.id}>{t.todo}</div>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import "./styles.css";
 
+// Interface for props passed to the InputField component
 interface Props {
-  todo: string;
-  setTodo: React.Dispatch<React.SetStateAction<string>>;
-  handleAdd: (e: React.FormEvent )=>void
+  todo: string; // Current todo input value
+  setTodo: React.Dispatch<React.SetStateAction<string>>; // Function to update todo input value
+  handleAdd: (e: React.FormEvent) => void; // Function to handle todo addition
 }
 
-const InputFeild: React.FC<Props> = ({ todo, setTodo, handleAdd}) => {
-
-  
-
+/**
+ * InputField component represents the input field for adding new todos.
+ * It receives todo value, function to update todo value, and function to handle todo addition as props.
+ */
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form className="input" onSubmit={handleAdd}>
+    <form className="input" onSubmit={(e) => handleAdd(e)}>
+      {/* Input field for entering new todo */}
       <input
-        type="text" 
+        type="text"
         placeholder="Enter A Task"
         className="input__box"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
       />
+      {/* Submit button to add new todo */}
       <button className="input_submit" type="submit">
         Go
       </button>
@@ -27,4 +31,4 @@ const InputFeild: React.FC<Props> = ({ todo, setTodo, handleAdd}) => {
   );
 };
 
-export default InputFeild;
+export default InputField;
