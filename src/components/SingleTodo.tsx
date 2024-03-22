b8f8ae2b5db8174d2cc90856294dab1eb16a796a
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-22 16:27:11 +1030

    done

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { SingleTodoProps } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -7,6 +7,12 @@ import "./styles.css";
 const SingleTodo: React.FC<SingleTodoProps> = ({ todo, todos, setTodos }) => {
   const [edit, setEdit] = useState<boolean>(false);
   const [editTodo, setEditTodo] = useState<string>(todo.todo);
+const inputRef = useRef<HTMLInputElement>(null)
+
+useEffect(() =>{
+
+  inputRef.current?.focus()
+},[edit])
 
   const handleDone = (id: number) => {
     setTodos(
@@ -28,14 +34,17 @@ setTodos(todos.map((todo) =>( todo.id === id?{...todo,todo:editTodo} : todo
 setEdit(false)
 }
 
+
   return (
     <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
        {edit ? (
             <input
+            ref={inputRef}
             placeholder="Edit Here"
               value={editTodo}
               onChange={(e) => setEditTodo(e.target.value)}
               className="todos__single--text"
+              
           
             />
       ) : todo.isDone ? (
