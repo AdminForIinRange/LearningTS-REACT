66c5b653573a438f5a7d2590ba9bda95285748b7
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-22 12:54:41 +1030

    apply Type safety to teh todos, and spreading todos and adding a new todo in the array

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React, { useState } from 'react'
 import './App.css'
 import InputFeild from './components/InputFeild'
+import { Todo } from './model'
 const App: React.FC = () => {
 
   const [todo, setTodo]=useState<string>("")
+  const [todos, setTodos]=useState<Todo[]>([])
 
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodo([...todos, {id: Date.now(),   }])
+    }
+
+   
+  };
   return (
     <div className='App'>
       <span className='heading'>Taskify</span>
 
-      <InputFeild todo={todo} setTodo={setTodo} />
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
       
       </div>
   )
--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -4,16 +4,15 @@ import "./styles.css";
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent )=>void
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-   
-  };
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd}) => {
+
+  
 
   return (
-    <form className="input" onSubmit={onSubmit}>
+    <form className="input" onSubmit={handleAdd}>
       <input
         type="text" 
         placeholder="Enter A Task"
